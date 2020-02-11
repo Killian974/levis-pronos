fcf21dabce39fce1cecbafa357f9d398d301507f
loulp <louislpogam@orange.fr>
2020-02-11 15:08:44 +0100

    Add homePage top section

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,32 @@
-test
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/homePage.css') }}">
+    <link rel="stylesheet" type="text/css" href="">
+    <title>Home Page</title>
+</head>
+<body>
+    <div class="topContainer">
+        <div>
+            <h1>Levis Pronos</h1>
+            <p>Lorem ipsum generatum lorum</p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+          <div class="col">
+            One of three columns
+          </div>
+          <div class="col">
+            One of three columns
+          </div>
+          <div class="col">
+            One of three columns
+          </div>
+        </div>
+      </div>
+</body>
+</html>
