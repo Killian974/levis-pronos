ff61a61b8d37b435e041b60bc21eaf3ffcfe1d24
loulp <louislpogam@orange.fr>
2020-02-13 10:01:40 +0100

    Add: footer

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/footer.css') }}">
+</head>
+<body>
+    <div class="footerContainer">
+        <div class="linkContainer">
+            <a href="https://www.google.com/search?q=google&oq=google&aqs=chrome..69i57j69i59j69i60l3j69i65l2j69i60.986j0j7&sourceid=chrome&ie=UTF-8" >Accueil</a>
+            <a href="https://www.google.com/search?q=google&oq=google&aqs=chrome..69i57j69i59j69i60l3j69i65l2j69i60.986j0j7&sourceid=chrome&ie=UTF-8" >Pronostique</a>
+            <a href="https://www.google.com/search?q=google&oq=google&aqs=chrome..69i57j69i59j69i60l3j69i65l2j69i60.986j0j7&sourceid=chrome&ie=UTF-8" >Contact</a>
+            <a href="https://www.google.com/search?q=google&oq=google&aqs=chrome..69i57j69i59j69i60l3j69i65l2j69i60.986j0j7&sourceid=chrome&ie=UTF-8" >Qui sommes nous ?</a>
+        </div>
+        <div class="imgContainer">
+            <img src="https://ressources.blogdumoderateur.com/2019/11/twitter-logo-1200x1200.jpg" class="networkImg" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/220px-Instagram_logo_2016.svg.png" class="networkImg" />
+            <img src="https://www.facebook.com/images/fb_icon_325x325.png" class="networkImg" />
+        </div>
+    </div> 
+</body>
+</html>
