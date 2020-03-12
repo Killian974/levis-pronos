a515783cf19970659641ee74a7d718074f7267e7
loulp <louislpogam@orange.fr>
2020-03-12 14:25:21 +0100

    Chg-admincss

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.pageDefault')
 
 @section('content')
-<div class="container">
+<div class="container" style="margin: 5% auto">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
